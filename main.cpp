86f240c00c0f89577eabbe25ac7ca74360984ca1
RiccardoSambito <117939705+RiccardoSambito@users.noreply.github.com>
2022-11-11 11:59:05 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,10 +3,21 @@ using namespace std;
 
 int main(){
 //Qui vanno definite le variabili
-
+float a, b, c;
 cin >> a >> b >> c;
 
-//Qui va scritto il codice per eseguire i calcoli richiesti
-
-
+//Qui va scritto il codice per eseguire i calcoli richiesta
+  float atringolo;
+  float aquadrato;
+  float arettangolo;
+  float atrapezio;
+  atriangolo= (a*b)/2;
+  aquadrato= a*a;
+  arettangolo= a*b;
+  atrapezio= ((a+b)*c)/2;
+  cout <<atriangolo<< endl;
+  cout <<aquadrato<< endl;
+  cout <<arettangolo<< endl;
+  cout <<atrapezio<< endl;
+  return 0;
 }
